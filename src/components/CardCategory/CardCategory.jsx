ab634ec1b2e7453b5037e7722fdc6ad11768ddc8
Sara Vázquez <saravazlo@gmail.com>
2025-07-10 21:39:15 +0200

    feat-scroll añadido

--- src/components/CardCategory/CardCategory.jsx
+++ src/components/CardCategory/CardCategory.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './CardCategory.css'
 
-const CardCategory = ({ category, image, title }) => {
+const CardCategory = ({ category, image, title, onClick }) => {
   return (
-    <div className={`card-category ${category}`}>
-    <h2 className="title">{title}</h2>
-    <div className="card-image-container">
-    <img src={image} alt={title} className="card-image" />
+<div className={`card-category card-category--${category}`} onClick={onClick}>
+    <h2 className="card-category__title">{title}</h2>
+    <div className="card-category__image-container">
+    <img src={image} alt={title} className="card-category__image" />
     </div>
     </div>
   )
