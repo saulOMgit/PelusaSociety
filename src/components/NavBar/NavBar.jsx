a908053c9a7a9445af42151a914273c39a278fb7
Sara Vázquez <saravazlo@gmail.com>
2025-07-10 11:46:43 +0200

    feat-boton favoritos sin estilos

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import './NavBar.css'
 import img from '../../assets/positivo.svg'
+import icon from '../../assets/fav.png'
 
 const NavBar = () => {
   return (
     <>
     <header className="navbar">
+        <div className="navbar__container">
         <div className="logo__container">
         <img src={img} alt="logotipo" className="logo"/>
+        </div>
+        <div className="buttonFavorites__container">
+            <img src={icon} alt="icono favoritos" className="icon"/>
+        <button className="buttonFavorites">Favoritos</button>
+        </div>
         </div>
     </header>
     </>
